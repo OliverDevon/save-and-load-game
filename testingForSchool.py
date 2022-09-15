d53767be585dfbf5e6b6ed87114064f51900e92e
OliverDevon <72664979+OliverDevon@users.noreply.github.com>
2022-09-15 18:02:14 +0100

    Add files via upload

---
+++ testingForSchool.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+name = "Oliver"
+name2 = "Todd"
+
+def save(num):
+    if num == 1:
+        list = [
+            name
+        ]
+        for items in list:
+            with open("save1", "w") as save1:
+                save1.write(items+"\n")
+    if num == 2:
+        list = [
+            name
+        ]
+        for items in list:
+            with open("save2", "w") as save2:
+                save2.write(items+"\n")
+
+def load(loadNum):
+    if loadNum == 1:
+        with open("save1", "r") as load1:
+            loadedName= load1.readlines()
+            name = loadedName[0][:-1]
+    if loadNum == 2:
+        with open("save2", "r") as load2:
+            loadedName= load2.readlines()
+            name = loadedName[0][:-1]
+
+
+try:
+    save(1)
+    load(1)
+    print(name2)
+except OSError:
+    print("can't find file / can't save file")
